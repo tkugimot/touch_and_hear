ba35faa259b23ad521e0be5c556bba2f4a2ae38f
Toshimitsu Kugimoto <49605004+tkugimot@users.noreply.github.com>
2023-08-04 09:06:58 +0900

    Add Firebase & Crashlytics (#3)
    * Add firebase
    
    * Add firebase_crashlytics
    
    * main.dart修正

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
+import firebase_crashlytics
 import soundpool_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   SwiftSoundpoolPlugin.register(with: registry.registrar(forPlugin: "SwiftSoundpoolPlugin"))
 }
